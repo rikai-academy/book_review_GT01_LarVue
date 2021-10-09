d69bcc1aec3ac5c13b893123af90d8a3c22379a5
Callmerey <levii.reys@gmail.com>
2021-10-09 21:03:25 +0700

    Login With Laravel Passport And VueJS Save Token To LocalStorage

--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/views/auth/Login'
+import Dashboard from '@/views/auth/Dashboard'
+import Register from '@/views/auth/Register'
 
 Vue.use(Router)
 
@@ -10,6 +13,23 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+      meta: { authOnly: true }
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+      meta: { guestOnly: true }
     }
   ]
 })
---
+++ Server/resources/lang/vn/message.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'register_success' => 'Đăng Kí Thành Công',
+    'register_fail' => 'Đăng Kí Không Thành Công',
+    'email_error' => 'Sai thông tin Gmail Hoặc Mật Khẩu',
+];
+
+?>
